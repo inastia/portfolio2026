81920c1534fa5e5745cc6a8bbbb1150ca5c9ca3d
nastia <nastia.bigun@gmail.com>
2026-02-20 19:26:40 -0700

    src + dist includison

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "src",
-  build: { outDir: "../dist" },
+  build: { outDir: "src/dist" },
   plugins: [react()],
   resolve: {
     alias: { "@": "/src" },
