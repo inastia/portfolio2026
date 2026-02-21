e63f6573227d9c1deff4418cce9213e4f0a47826
nastia <nastia.bigun@gmail.com>
2026-02-20 19:21:25 -0700

    fix for vercel + vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-root: "src",
-plugins: [react()],
-resolve: {
-alias: { "@": "/src" },
-},
+  root: "src",
+  build: { outDir: "../dist" },
+  plugins: [react()],
+  resolve: {
+    alias: { "@": "/src" },
+  },
 });
