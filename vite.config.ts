4df1ffb29ee2f8082380247e4be5e7a9eacfd19b
nastia <nastia.bigun@gmail.com>
2026-02-23 15:04:17 -0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   root: "src",
   build: { outDir: "src/dist" },
-  plugins: [react()],
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean,
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
